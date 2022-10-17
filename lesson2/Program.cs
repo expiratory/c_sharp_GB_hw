7090ce36fd8b9823c08e836d584433469b87c5a0
Aleksandr_Kilchevskiy <alexkill94@mail.ru>
2022-10-17 22:12:07 +0300

    fixed smth

--- lesson2/Program.cs
+++ lesson2/Program.cs
@@ -19,35 +19,61 @@
 // Console.WriteLine(result);
 
 // Программа принимает на вход два числа и выводит, является ли второе число кратным первому
-// Console.WriteLine("Введите первое число: ");
-// int number1 = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Введите второе число: ");
-// int number2 = Convert.ToInt32(Console.ReadLine());
-// int ostatok = number1%number2;
-
-// if (ostatok == 0)
-// {
-//     Console.WriteLine($"Число {number2} кратно {number1}");
-// }
-// else
-// {
-//     Console.WriteLine($"Число {number2} не кратно {number1}, остаток {ostatok}");
-// }
-
-// Является ли одно число квадратом другого
 Console.WriteLine("Введите первое число: ");
 int number1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число: ");
 int number2 = Convert.ToInt32(Console.ReadLine());
 
-if(number1 == Math.Pow(number2, 2) | number2 == Math.Pow(number1, 2))
+while (number1 == 0 || number2 == 0)
 {
-    Console.WriteLine("Одно из чисел является квадратом другого");
+    Console.WriteLine("Введите первое число: ");
+    number1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите второе число: ");
+    number2 = Convert.ToInt32(Console.ReadLine());
+    if (number1 != 0 && number2 != 0)
+    {
+        break;
+    }
+}
+
+int ostatokBolshe = number1%number2;
+int ostatokMenshe = number2%number1;
+if (number1>number2)
+{
+    if (ostatokBolshe == 0)
+    {
+        Console.WriteLine($"Число {number2} кратно {number1}");
+    }
+    else
+    {
+        Console.WriteLine($"Число {number1} не кратно {number2}, остаток {ostatokBolshe}");
+    }
 }
 else
 {
-    Console.WriteLine("Ни одно из чисел не является квадратом другого");
+    if (ostatokMenshe == 0)
+    {
+        Console.WriteLine($"Число {number1} кратно {number2}");
+    }
+    else
+    {
+        Console.WriteLine($"Число {number2} не кратно {number1}, остаток {ostatokMenshe}");
+    }    
 }
+// Является ли одно число квадратом другого
+// Console.WriteLine("Введите первое число: ");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число: ");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+
+// if(number1 == Math.Pow(number2, 2) | number2 == Math.Pow(number1, 2))
+// {
+//     Console.WriteLine("Одно из чисел является квадратом другого");
+// }
+// else
+// {
+//     Console.WriteLine("Ни одно из чисел не является квадратом другого");
+// }
 
 // Делится ли число одновременно на 7 и на 23
 // Console.WriteLine("Введите число: ");
