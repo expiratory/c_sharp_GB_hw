b447165d1322470f544783fab584d9f81355070d
Aleksandr_Kilchevskiy <alexkill94@mail.ru>
2022-10-18 00:41:08 +0300

    task 15 done

--- homework_lesson2/Program.cs
+++ homework_lesson2/Program.cs
@@ -26,37 +26,70 @@
 // }
 
 // Задача 13 - выводит третью цифру заданного числа или сообщает, что третьей цифры нет
-Console.WriteLine("Введите число: ");
-int number = Convert.ToInt32(Console.ReadLine());
-number = Math.Abs(number);
+// Console.WriteLine("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// number = Math.Abs(number);
 
-int numberTo123(int chislo)
-{
-    while (chislo >= 1000)
-        {
-            chislo = chislo/10;
-        }
-    number = chislo;
-    return number;
-}
+// int numberTo123(int chislo)
+// {
+//     while (chislo >= 1000)
+//         {
+//             chislo = chislo/10;
+//         }
+//     number = chislo;
+//     return number;
+// }
 
-if (number > 99)
+// if (number > 99)
+// {
+//     while (true)
+//     {
+//         if (number > 99 && number < 1000)
+//         {
+//             int number3 = number%10;
+//             Console.WriteLine(number3);
+//             break;
+//         }
+//         else
+//         {
+//             numberTo123(number);
+//         }
+//     }
+// }
+// else
+// {
+//     Console.WriteLine("Третьей цифры нет");
+// }
+
+// Задача 15 - принимает цифру, обозначающую день недели, и проверяет, является ли день выходным
+Console.WriteLine("введите цифру от 1 до 7, обозначающую день недели: ");
+int dayOfTheWeek = Convert.ToInt32(Console.ReadLine());
+
+while (true)
 {
-    while (true)
+    if (dayOfTheWeek >= 1 && dayOfTheWeek <= 7)
     {
-        if (number > 99 && number < 1000)
+        if (dayOfTheWeek >= 1 && dayOfTheWeek <= 5)
         {
-            int number3 = number%10;
-            Console.WriteLine(number3);
+            Console.WriteLine("Этот день не является выходным");
             break;
         }
         else
         {
-            numberTo123(number);
+           Console.WriteLine("Этот день является выходным");
+           break; 
         }
     }
-}
-else
-{
-    Console.WriteLine("Третьей цифры нет");
+    else
+    {
+        while (dayOfTheWeek < 1 || dayOfTheWeek > 7)
+        {
+            Console.WriteLine("Нужно ввести цифру от 1 до 7. Введите еще раз: ");
+            dayOfTheWeek = Convert.ToInt32(Console.ReadLine());
+            if (dayOfTheWeek >= 1 && dayOfTheWeek <= 7)
+            {
+                break;
+            }
+        }
+    }
 }
